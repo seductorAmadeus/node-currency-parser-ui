c8bc3da29000b3896e0b87f921ebd5a28e24d814
seductorAmadeus <seductorAmadeus@gmail.com>
2020-01-28 13:25:56 +0300

    remove material-ui lib, add simple table

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Table data={currencyData} />
+        <Table currency={currencyData} />
       </header>
     </div>
   );
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,42 +1,99 @@
-import React from 'react';
-import MaterialTable from 'material-table';
+import React, {Component} from 'react';
+import './Table.css';
+import PropTypes from 'prop-types';
 
-export default function Table(props) {
-  const {data: rows} = props.data;
+const sortTypes = {
+  up: {
+    class: 'sort-up',
+    fn: (a, b) => a.usd_price - b.usd_price || a.last_updated - b.last_updated,
+  },
+  down: {
+    class: 'sort-down',
+    fn: (a, b) => b.usd_price - a.usd_price || b.last_updated - a.last_updated,
+  },
+  default: {
+    class: 'sort',
+    fn: (a, b) => a,
+  },
+};
 
-  if (!rows || rows.length <= 0) {
-    return null;
+export default class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSort: 'default',
+    };
   }
 
-  return (
-    rows &&
-    rows.length > 0 && (
-      <MaterialTable
-        columns={[
-          {title: 'ID', field: 'id'},
-          {title: 'Currency name', field: 'name'},
-          {title: 'USD price', field: 'usd_price'},
-          {title: 'Date', field: 'last_updated'},
-          {title: 'Volume 24h', field: 'volume_24h'},
-          {title: '% change 1h', field: 'percent_change_1h'},
-          {title: '% change 24h', field: 'percent_change_24h'},
-          {title: '% change 7d', field: 'percent_change_7d'},
-        ]}
-        data={rows.map(item => {
-          const tableData = {
-            id: item.id,
-            name: item.name,
-            usd_price: item.usd_price,
-            last_updated: item.last_updated,
-            volume_24h: item.volume_24h,
-            percent_change_1h: item.percent_change_1h,
-            percent_change_24h: item.percent_change_24h,
-            percent_change_7d: item.percent_change_7d,
-          };
-          return tableData;
-        })}
-        title="Измерения"
-      />
-    )
-  );
+  onSortChange = () => {
+    const {currentSort} = this.state;
+    let nextSort;
+
+    if (currentSort === 'down') nextSort = 'up';
+    else if (currentSort === 'up') nextSort = 'default';
+    else if (currentSort === 'default') nextSort = 'down';
+
+    this.setState({
+      currentSort: nextSort,
+    });
+  };
+
+  render() {
+    const {currentSort} = this.state;
+    const {currency} = this.props;
+    const {data} = currency;
+
+    if (!data || data.length <= 0) {
+      return <div>wait...</div>;
+    }
+
+    return (
+      data &&
+      data.length > 0 && (
+        <div id="results" className="wrap__table table ">
+          <div className="row header__table">
+            <div className="cell">ID</div>
+            <div className="cell">Currency name</div>
+            {/* eslint-disable-next-line jsx-a11y/interactive-supports-focus */}
+            <div
+              role="button"
+              className={`cell active-sort ${sortTypes[currentSort].class}`}
+              onClick={this.onSortChange}
+              onKeyPress={this.onSortChange}>
+              USD price
+            </div>
+            {/* eslint-disable-next-line jsx-a11y/interactive-supports-focus */}
+            <div
+              role="button"
+              className={`cell active-sort ${sortTypes[currentSort].class}`}
+              onClick={this.onSortChange}
+              onKeyPress={this.onSortChange}>
+              Date
+            </div>
+            <div className="cell">Volume 24h</div>
+            <div className="cell">% change 1h</div>
+            <div className="cell">% change 24h</div>
+            <div className="cell">% change 7d</div>
+          </div>
+
+          {[...data].sort(sortTypes[currentSort].fn).map(item => (
+            <div className="row" key={item.id}>
+              <div className="cell">{item.id}</div>
+              <div className="cell">{item.name}</div>
+              <div className="cell">{item.usd_price}</div>
+              <div className="cell">{item.last_updated}</div>
+              <div className="cell">{item.volume_24h}</div>
+              <div className="cell">{item.percent_change_1h}</div>
+              <div className="cell">{item.percent_change_24h}</div>
+              <div className="cell">{item.percent_change_7d}</div>
+            </div>
+          ))}
+        </div>
+      )
+    );
+  }
 }
+
+Table.propTypes = {
+  currency: PropTypes.object.isRequired,
+};
